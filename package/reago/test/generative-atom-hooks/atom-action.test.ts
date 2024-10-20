4cc6cc823b6f1e2d3338597cf3563086c83ff69a
Sebastian Nowak <sebastian-nowak@users.noreply.github.com>
2024-10-20 17:52:30 +0200

    test: cover the case when dispatch() is run on an atom that's currently computing

---
+++ package/reago/test/generative-atom-hooks/atom-action.test.ts
@@ -0,0 +1,35 @@
+// =============================================================================
+// atomAction generative atom tests
+// =============================================================================
+
+import {atomAction, dispatch, read} from 'reago';
+import {expect, test} from 'vitest';
+
+
+test('atomAction handler will run even if it was not reached when dispatch() was invoked', async () => {
+  let registered = false;
+  let triggered = false;
+
+  function* $atom() {
+    yield Promise.resolve(123);
+    registered = true;
+    atomAction(() => {
+      triggered = true;
+    }, []);
+  }
+
+  // run everything up to the first yield
+  const promise = read($atom);
+  expect(registered).toBeFalsy();
+  expect(triggered).toBeFalsy();
+
+  // trigger a dispatch but it doesn't see the atomAction yet
+  dispatch($atom)();
+  expect(registered).toBeFalsy();
+  expect(triggered).toBeFalsy();
+
+  // when computation finishes, the dispatch will run
+  await promise;
+  expect(registered).toBeTruthy();
+  expect(triggered).toBeTruthy();
+});
