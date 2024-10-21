e4e563477c9bf5268469167262c1ca0c5f3c7cb3
Sebastian Nowak <sebastian-nowak@users.noreply.github.com>
2024-10-21 18:26:02 +0200

    fix: `changeset` expects an upper-case CHANGELOG.md file

--- script/ci-changeset-publish.sh
+++ script/ci-changeset-publish.sh
@@ -5,5 +5,16 @@ if [[ -z "$CI" ]]; then
   exit 1
 fi
 
+# build everything
 npm run build --workspaces --if-present
+
+# `changeset` expects an upper-case CHANGELOG.md file
+if ! [ -f .NPMRC ]; then
+  # case-sensitive fs
+  find . -type f -name "changelog.md" -execdir cp {} CHANGELOG.md \;
+  git config --global core.excludesfile ~/.gitignore
+  echo "CHANGELOG.md" >> ~/.gitignore
+fi
+
+# publish changed packages
 npx changeset publish
--- script/ci-changeset-version.sh
+++ script/ci-changeset-version.sh
@@ -5,14 +5,22 @@ if [[ -z "$CI" ]]; then
   exit 1
 fi
 
-npx changeset version
-
+# `changeset` expects an upper-case CHANGELOG.md file
 if ! [ -f .NPMRC ]; then
   # case-sensitive fs
-  find . -type f -iname "CHANGELOG.md" -execdir cp {} changelog.md \;
+  find . -type f -name "changelog.md" -execdir mv {} CHANGELOG.md \;
+fi
+
+# consume changesets and set new versions
+npx changeset version
+
+# force lower-case file name for changelog.md
+if ! [ -f .NPMRC ]; then
+  # case-sensitive fs
+  find . -type f -name "CHANGELOG.md" -execdir cp {} changelog.md \;
   git config --global core.excludesfile ~/.gitignore
   echo "CHANGELOG.md" >> ~/.gitignore
 else
   # case-insensitive fs
-  find . -type f -iname "CHANGELOG.md" -execdir mv {} changelog.md \;
+  find . -type f -name "CHANGELOG.md" -execdir mv {} changelog.md \;
 fi
